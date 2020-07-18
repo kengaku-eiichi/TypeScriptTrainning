8a5a4af6ed2830c854bb41748944f8673a8f5ecf
kengaku <keng9358@gmail.com>
2020-07-18 22:12:49 +0900

    alias 追加

--- centos8/etc/profile.d/tsc.sh
+++ centos8/etc/profile.d/tsc.sh
@@ -1,1 +1,3 @@
 alias tsc='docker run --rm -it -v ${PWD}:${PWD} -w ${PWD} typescripttrainning_tsc3.9.7 tsc'
+alias node='docker run --rm -it -v ${PWD}:${PWD} -w ${PWD} typescripttrainning_tsc3.9.7 node'
+alias npm='docker run --rm -it -v ${PWD}:${PWD} -w ${PWD} typescripttrainning_tsc3.9.7 npm'
