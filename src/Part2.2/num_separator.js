e0ee52ec9fd9a44b51989b955b7c0cd94fc78d16
kengaku <keng9358@gmail.com>
2020-07-18 23:01:08 +0900

    Part2.2

---
+++ src/Part2.2/num_separator.js
@@ -0,0 +1,6 @@
+{
+    var num = 1000000;
+    var num2 = 15900;
+    var num3 = 1234567;
+    var num4 = 2132;
+}
---
+++ src/Part2.2/str_template.ts
@@ -0,0 +1,8 @@
+{
+    let mail: string = 'admin@example.com'
+    let msg = `レビュアー募集中！
+
+書籍の感想を是非お聞かせください。
+弊社サポートサイト、SNSなどで紹介させていただきます。（送付先：${mail}）`
+    console.log(msg)
+}
