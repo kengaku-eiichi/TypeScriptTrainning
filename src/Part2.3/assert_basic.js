d8af52c7789ff3156fce8c2eade4ec126972e3bc
kengaku <keng9358@gmail.com>
2020-07-18 23:32:57 +0900

    Part2.3

---
+++ src/Part2.3/assert_basic.js
@@ -0,0 +1,8 @@
+{
+    function show(result) {
+        return "\u7D50\u679C\u306F" + result + "\u3067\u3059\u3002";
+    }
+    // console.log(show(100))
+    console.log(show(100));
+    console.log(show(100));
+}
---
+++ src/Part2.3/assert_error.js
@@ -0,0 +1,4 @@
+function toInt(value) {
+    return value.toFixed(0);
+}
+console.log(toInt('hoge'));
