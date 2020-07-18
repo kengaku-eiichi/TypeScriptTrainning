8988171c81e5e1199663643482df793d891e7575
kengaku <keng9358@gmail.com>
2020-07-19 02:00:47 +0900

    Part3.2 連想配列

---
+++ src/Part3.2/assoc_basic.js
@@ -0,0 +1,10 @@
+{
+    var obj = {
+        'hoge': 'ほげ',
+        'foo': 'ふー',
+        'bar': 'ばー'
+    };
+    console.log(obj.hoge);
+    console.log(obj['foo']);
+    console.log(obj);
+}
---
+++ src/Part3.2/assoc_basic.ts
@@ -0,0 +1,10 @@
+{
+    let obj: { [index: string]: string } = {
+        'hoge': 'ほげ',
+        'foo': 'ふー',
+        'bar': 'ばー'
+    }
+    console.log(obj.hoge)
+    console.log(obj['foo'])
+    console.log(obj)
+}
