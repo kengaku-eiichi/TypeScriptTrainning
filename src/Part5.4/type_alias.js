7701f59cba3d8923f6b5591f4a9c9a0684016996
kengaku <keng9358@gmail.com>
2020-07-20 00:44:42 +0900

    Part5.4 型エイリアス

---
+++ src/Part5.4/type_alias.js
@@ -0,0 +1,7 @@
+{
+    // let data: FooType = ['abc', '1', true]
+    var data = ['abc', 1, true];
+    var hoge = 'hoge';
+    var fuga = true;
+    // let one: Hoge = 1
+}
---
+++ src/Part5.4/type_alias.ts
@@ -0,0 +1,10 @@
+{
+    type FooType = [string, number, boolean]
+    // let data: FooType = ['abc', '1', true]
+    let data: FooType = ['abc', 1, true]
+
+    type Hoge = string | boolean
+    let hoge: Hoge = 'hoge'
+    let fuga: Hoge = true
+    // let one: Hoge = 1
+}
